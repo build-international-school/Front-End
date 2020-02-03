5dccff62c0a9a676f2d1108d7ab8afaf1a50f8e7
Lambda_School_Loaner_252 <c0der.br1t@gmail.com>
2020-02-03 12:25:07 -0600

    Add dependencies (redux, react-redux, redux-thunk, redux-logger). Create basic reducer in reducers folder. Create store in index.js. Wrap <App> in provider.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,10 @@
 // Reducers index.js
 
+const initialState = {};
+
+export const reducer = (state= initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
