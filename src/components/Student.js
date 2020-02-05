093cc5ee87ebed08f8df64094439d7e91e87b9fa
Mandi Haase <haase1020@gmail.com>
2020-02-05 13:01:37 -0500

    add Student.js component

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,21 +1,42 @@
 // // Individual student information (edit and delete will live here too)
 import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useParams } from 'react-router-dom';
 
-const { first_name, last_name, grade, age, status} =  () => {
+const Student =(props) => {
+    const [student, setStudent] = useState();
+    let {id} = useParams();
+
+     useEffect(() =>{
+
+        axiosWithAuth
+        .get(`https://issw.herokuapp.com/api/students/${id}`)
+        
+        .then(response => {
+            console.log("Here is the response",response)
+            setStudent(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    },[id]);
+
+
+
+const { first_name, last_name, grade, age, status } = student; 
 //     console.log("In Student: ", props.student);
     return (
         <div className="save-wrapper">
             <div className="student-card">
-                <h2></h2>
-            This is a student.
-        </div>
-//         <div>
-//            <h1>Student</h1>
-//            <div>
-//                <h2>{props.student.first_name} {props.student.last_name}</h2>
-//            </div>
-//         </div>
+                <h2>{first_name}</h2>
+                <h2>{last_name}</h2>
+                <p>grade: {grade}</p>
+                <p>age: {age}</p>
+                <p>status:{status}</p>
+            </div>
+        </div> 
     )
-}
+    
+    }
 
 export default Student;
