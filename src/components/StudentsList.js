7d809551c831f4d0e434e5c563d493a38156e89c
Mandi Haase <haase1020@gmail.com>
2020-02-06 09:33:28 -0500

    changes to StudentsList.js

--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -3,47 +3,11 @@ import { Link, Route } from 'react-router-dom';
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import Student from "./Student";
 
-// const dummyStudents =[
-//     {
-//         id: 1,
-//         first_name: 'Mandi',
-//         last_name: 'Haase',
-//         grade: '3',
-//         address: '111 Ever St., Alexandria, VA 00000',
-//         img_url: '',
-//         background: 'I have been learning programming for 2 months',
-//         status: 'active',
-//         age: '10',
-//         insurance: false,
-//         exp_date: '',
-//         birth_certificate: true,
-//         special_needs: 'no',
-//         representative_name: '',
-//         representative_contact: ''
-//     },
 
-//     {
-//         id: 2,
-//         first_name: 'Jon',
-//         last_name: 'Haase',
-//         grade: '4',
-//         address: '222 Ever St., Alexandria, VA 00000',
-//         img_url: '',
-//         background: 'I have been learning programming for 3 months',
-//         status: 'active',
-//         age: '11',
-//         insurance: false,
-//         exp_date: '',
-//         birth_certificate: true,
-//         special_needs: 'no',
-//         representative_name: '',
-//         representative_contact: ''
-//     }
-// ]
 
 const Students =() => {
     const [students, setStudents] =useState([]);
-    // const [searchResults, setSearchResults] =useState([])
+
 
 
 useEffect(()=> {
@@ -71,34 +35,6 @@ console.log("students",students)
     )
     )}
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* {students.map(student => (
-            <Link to={`/student/${student.id}`}>
-        <Student data={student}/>
-        </Link> */}
-    
-    
-        {/* ))};
-        <Route path={"/student/:id"} component={Student}/> */}
     
 </div>
     )
